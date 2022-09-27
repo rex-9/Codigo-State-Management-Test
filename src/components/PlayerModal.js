378f5075450bf782618c9076ae6d4e961188200b
Htet Naing <htetnaing0814@gmail.com>
2022-09-28 00:08:58 +0630

    trying to fix this error for over 2 hours

--- src/components/PlayerModal.js
+++ src/components/PlayerModal.js
@@ -1,4 +1,3 @@
-import { parseSync } from '@babel/core';
 import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { createJoin } from '../redux/join';
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,11 @@
 const Login = () => (
-  <>Login</>
+  <>
+    <div>
+      <h1>Login</h1>
+      <label htmlFor="Username">Username <span className="bg-red-500">*</span></label>
+      <input type="text" placeholder="Username" name="Username" />
+    </div>
+  </>
 );
 
 export default Login;
