d79a16ca60252726bc1a2b779bb7f20dc0a7b8c5
Htet Naing <htetnaing0814@gmail.com>
2022-09-28 08:52:47 +0630

    commented

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -5,19 +5,18 @@ import playerReducer from './player';
 import teamReducer from './team';
 import joinReducer from './join';
 
+
+// combined reducers
 const rootReducer = combineReducers({
   players: playerReducer,
   teams: teamReducer,
   joins: joinReducer,
 });
 
+// redux store
 const store = configureStore({
   reducer: rootReducer,
   middleware: [logger, thunk],
 });
 
-export const setupStore = (preloadedState) => configureStore({
-  reducer: rootReducer,
-  preloadedState,
-});
 export default store;
